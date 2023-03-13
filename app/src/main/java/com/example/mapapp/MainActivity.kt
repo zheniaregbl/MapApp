fc735af2ad93a4a8188fd9c5061217047639f123
zhenia_regbl <zhenieregbl@gmail.com>
2023-03-13 22:27:22 +0300

    Change variable gg

--- app/src/main/java/com/example/mapapp/MainActivity.kt
+++ app/src/main/java/com/example/mapapp/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         var l: String = "Hello";
         var thrid: Int = 3;
         val hello: String = "Hello Man"
+        var gg: String = "Eto gg"
     }
 }
