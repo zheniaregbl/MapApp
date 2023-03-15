408df7b1cebd2d669785e38f7b6ad857a4c026f3
zhenia_regbl <zhenieregbl@gmail.com>
2023-03-15 19:53:08 +0300

    Get started

--- app/src/main/java/com/example/mapapp/MainActivity.kt
+++ app/src/main/java/com/example/mapapp/MainActivity.kt
@@ -2,15 +2,36 @@ package com.example.mapapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mapapp.databinding.ActivityMainBinding
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        var l: String = "Hello";
-        var thrid: Int = 3;
-        val hello: String = "Hello Man"
-        var gg: String = "Eto gg"
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        MapKitFactory.setApiKey("2ba9190a-03d8-47a7-a391-4983fe6eecf8")
+        MapKitFactory.initialize(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        super.onCreate(savedInstanceState)
+        binding.mapView.map.move(CameraPosition(Point(58.0, 31.2), 11.0f, 0.0f, 0.0f),
+        Animation(Animation.Type.SMOOTH, 300f), null)
+    }
+
+    override fun onStop() {
+        binding.mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onStart() {
+        binding.mapView.onStart()
+        MapKitFactory.getInstance().onStart()
+        super.onStart()
     }
 }
