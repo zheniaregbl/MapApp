ee61636ab01a8dba519aba8fbe288e4a0e7cf90b
zhenia_regbl <zhenieregbl@gmail.com>
2023-05-16 08:04:00 +0300

    Add event onMapLongTap

--- app/src/main/java/com/example/mapapp/MainActivity.kt
+++ app/src/main/java/com/example/mapapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mapapp
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.PointF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils.loadAnimation
+import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.children
 import com.example.mapapp.databinding.ActivityMainBinding
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
@@ -18,12 +25,18 @@ import com.yandex.mapkit.user_location.UserLocationLayer
 import com.yandex.mapkit.user_location.UserLocationObjectListener
 import com.yandex.mapkit.user_location.UserLocationView
 import com.yandex.runtime.image.ImageProvider
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
-class MainActivity : AppCompatActivity(), UserLocationObjectListener, CameraListener {
+class MainActivity : AppCompatActivity(), UserLocationObjectListener, CameraListener, InputListener {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var currentLocation: UserLocationLayer
+    private lateinit var inAnimation: android.view.animation.Animation
+    private lateinit var outAnimation: android.view.animation.Animation
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         MapKitFactory.setApiKey("2ba9190a-03d8-47a7-a391-4983fe6eecf8")
         MapKitFactory.initialize(this)
@@ -31,12 +44,18 @@ class MainActivity : AppCompatActivity(), UserLocationObjectListener, CameraList
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        inAnimation = loadAnimation(this, R.anim.alpha_in)
+        outAnimation = loadAnimation(this, R.anim.alpha_out)
+
         hideSystemUI()
+        initToggleButton()
 
         super.onCreate(savedInstanceState)
 
         binding.mapView.map.move(CameraPosition(Point(58.4, 31.2), 11.0f, 0.0f, 0.0f),
         Animation(Animation.Type.SMOOTH, 0f), null)
+
+        binding.mapView.map.addInputListener(this)
 
         val style: String = "[" +
                 "        {" +
@@ -60,6 +79,28 @@ class MainActivity : AppCompatActivity(), UserLocationObjectListener, CameraList
         currentLocation = mapKit.createUserLocationLayer(binding.mapView.mapWindow)
         currentLocation.isVisible = true
         currentLocation.setObjectListener(this)
+
+        binding.imageFilter.setOnClickListener {
+            if(binding.layoutFilter.visibility == View.VISIBLE){
+                binding.layoutFilter.startAnimation(outAnimation)
+                binding.layoutFilter.visibility = View.GONE
+                binding.cardViewFilter.setCardBackgroundColor(Color.TRANSPARENT)
+            }
+            else{
+                binding.cardViewFilter.setCardBackgroundColor(Color.parseColor("#1B1B1B"))
+                binding.layoutFilter.visibility = View.VISIBLE
+                binding.layoutFilter.startAnimation(inAnimation)
+            }
+        }
+    }
+
+    override fun onMapTap(map: Map, point: Point) {
+        TODO("Nothing")
+    }
+
+    override fun onMapLongTap(p0: Map, point: Point) {
+        Log.d("tag123", "${(point.latitude * 1000.0).roundToInt() / 1000.0} " +
+                "${(point.longitude * 1000.0).roundToInt()  / 1000.0}")
     }
 
     override fun onStop() {
@@ -116,6 +157,18 @@ class MainActivity : AppCompatActivity(), UserLocationObjectListener, CameraList
     ) {
         if (finish) {
             currentLocation.resetAnchor()
+        }
+    }
+
+    private fun initToggleButton() = with(binding){
+        tgBtAll.setOnCheckedChangeListener { _, _ ->
+            if (tgBtAll.isChecked){
+                tgBtMonument.isChecked = false
+                tgBtRel.isChecked = false
+                tgBtPark.isChecked = false
+                tgBtHotel.isChecked = false
+                tgBtCafe.isChecked = false
+            }
         }
     }
 
